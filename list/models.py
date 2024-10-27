64316fce3f41edb004f7bbbe3c9a88661ac0020c
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-10-27 16:43:36 +0100

    create models in models.py

--- list/models.py
+++ list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.content
