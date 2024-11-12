768d7720dee75df3ae5784e453a87fdd8e0cee07
Anton Hryhorenko <anton.h.sind@gmail.com>
2024-11-12 18:46:45 +0100

    Added new custom form for Tasks

---
+++ list/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ["content", "deadline", "tags"]
+        widgets = {
+            'deadline': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
